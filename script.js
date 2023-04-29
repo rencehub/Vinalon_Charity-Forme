d652b5dc7454869a37c81dcaace95fefbe537f44
rencehub <126467038+rencehub@users.noreply.github.com>
2023-04-29 23:48:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Display warning message for required fields
+var form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    var requiredFields = document.querySelectorAll('input[required]');
+    var emptyFields = Array.from(requiredFields).filter(function(field) {
+        return !field.value;
+    });
+
+    if (emptyFields.length > 0) {
+        event.preventDefault();
+        alert('Please fill in all required fields.');
+    }
+});
